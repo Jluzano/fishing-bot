640eac9a68d82bc23f629b086d4b7cdbf47012a2
Jluzano <justinluzano@gmail.com>
2023-08-22 20:10:20 -0700

    reverting to previous original code

--- fish.py
+++ fish.py
@@ -1,50 +1,52 @@
+import os.path
 import keyboard
 import mss
+import mss.tools
 import time
+import pyautogui
+import json
+import os
 
-# Color values for arrow keys
-ARROW_COLORS = {
-    (245, 197, 67): "LEFT",
-    (52, 145, 247): "DOWN",
-    (226, 50, 50): "UP",
-    (45, 236, 43): "RIGHT",
-    (174, 49, 209): "SPACE"
-}
+with mss.mss() as sct:
+    # Get information of monitor 2
+    monitor_number = 1
+    mon = sct.monitors[monitor_number]
+    start_time = time.time()
 
-def main():
-    with mss.mss() as sct:
-        monitor_number = 1
-        mon = sct.monitors[monitor_number]
+    # (1120, 710)
+    x = 55
+    y = 47
 
-        x = 55
-        y = 47
+    # The screen part to capture
+    monitor = {
+        "top": mon["top"] + 665,  # 100px from the top
+        "left": mon["left"] + 1073,  # 100px from the left
+        "width": 1280,
+        "height": 720,
+        "mon": monitor_number,
+    }
 
-        monitor = {
-            "top": mon["top"] + 665,
-            "left": mon["left"] + 1073,
-            "width": 1280,
-            "height": 720,
-            "mon": monitor_number,
-        }
+    #print("Starting in 5...")
+    #time.sleep(5)
+    
+    while True:
+        sct_img = sct.grab(monitor)
+        pixel_color = sct_img.pixel(x, y)  # Get the color value at (1120, 710)
 
-        start_time = time.time()
-        fps_counter = 0
-
-        while True:
-            sct_img = sct.grab(monitor)
-            pixel_color = sct_img.pixel(x, y)
-
-            arrow_key = ARROW_COLORS.get(pixel_color, "NONE")
-            print(arrow_key)
-
-            if keyboard.is_pressed('q'):
-                break
-            
-            fps_counter += 1
-            if time.time() - start_time >= 1:
-                print("FPS:", fps_counter)
-                start_time = time.time()
-                fps_counter = 0
-
-if __name__ == "__main__":
-    main()
+        # Check if the RGB values match black (0, 0, 0)
+        if pixel_color == (245, 197, 67): # Arrow left
+            print("LEFT")
+        elif pixel_color == (52, 145, 247): # Arrow down
+            print("DOWN")
+        elif pixel_color == (226, 50, 50): # Arrow down
+            print("UP")
+        elif pixel_color == (45, 236, 43): # Arrow down
+            print("RIGHT")
+        elif pixel_color == (174, 49, 209): # Arrow down
+            print("SPACE")
+        else:
+            print("NONE")
+        
+        if keyboard.is_pressed('q'):
+            break
+        print("FPS: ", 1.0 / (time.time() - start_time))
